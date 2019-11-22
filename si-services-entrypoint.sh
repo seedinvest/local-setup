8b9bba7d187de5608bd49760fd14fec4098caa70
Ryan Brink <rbrink@circle.com>
2019-11-22 14:55:03 -0500

    postgres health check

--- si-services-entrypoint.sh
+++ si-services-entrypoint.sh
@@ -4,6 +4,12 @@ set -e
 source seedinvest/settings/common_environment.sh
 source seedinvest/settings/local_environment.sh
 
+while ! pg_isready -h postgres
+do
+    echo "$(date) - waiting for database to start"
+    sleep 10s
+done
+
 ./manage.py runserver 0.0.0.0:8000 --settings=seedinvest.settings.local_common
 
 exec "$@"
